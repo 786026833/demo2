e93daaa173a4d3e2ec0f058f7eefc29be707a576
PC201602051050\Administrator <786026833@qq.com>
2018-04-19 11:42:25 +0800

    ss

--- src/Test3.java
+++ src/Test3.java
@@ -1,7 +1,8 @@
-package PACKAGE_NAME;
-
 /**
  * Created by Administrator on 2018/4/19.
  */
 public class Test3 {
+    public static void main(String[] args) {
+        System.out.println("26444444444444444444");
+    }
 }
